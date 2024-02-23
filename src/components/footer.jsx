6d525971bf7e563a2d24ff27d75bb09170ba3f34
MwangiNelson <nelsonmwangi197@gmail.com>
2024-02-23 14:26:32 +0300

    changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,5 +1,6 @@
+import { Tooltip } from "flowbite-react";
 import React from "react";
-import { RiGithubLine, RiGoogleLine, RiMailAddFill, RiTwitterLine } from "react-icons/ri";
+import { RiGithubFill, RiGithubLine, RiGoogleLine, RiMailAddFill, RiTwitterLine } from "react-icons/ri";
 
 
 function Footer() {
@@ -7,37 +8,20 @@ function Footer() {
         <footer className="bg-primary rounded-b-md px-5 sticky bottom-0 dark:bg-gray-800 w-full hidden md:flex justify-center items-center">
             <div className="  px-5 py-1 md:p-4 md:flex md:items-center w-full items-end md:justify-between lg:justify-between">
                 <ul className="hidden md:flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-                    <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">
-                            Team
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">
-                            Data
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline me-4 md:me-6">
-                            Source Code
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" className="hover:underline">
-                            Documentation
-                        </a>
-                    </li>
+     
+ 
                 </ul>
                 <span className="text-sm flex flex-row gap-5 justify-center items-center text-gray-500 sm:text-center dark:text-gray-400">
-                    <a href="">
-                        <RiGithubLine className="text-sm md:text-3xl text-black" />
-                    </a>
-                    <a href="">
-                        <RiTwitterLine className="text-sm md:text-3xl text-black" />
-                    </a>
-                    <a href="">
-                        <RiMailAddFill className="text-sm md:text-3xl text-black" />
-                    </a>
+                    <Tooltip content="Github Link">
+                        <a href="https://github.com/Mama-Frontida" target="_blank">
+                            <RiGithubFill className="text-sm md:text-4xl text-black" />
+                        </a>
+                    </Tooltip>
+                    <Tooltip content='Huggingface' style="dark">
+                        <a href="https://huggingface.co/Danroy/MamaFrontida" target="_blank" className="text-3xl">
+                            🤗
+                        </a>
+                    </Tooltip>
 
                 </span>
 
